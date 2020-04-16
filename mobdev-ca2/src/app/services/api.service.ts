b8a4a304388c9e1492a82a2f3072bbdb2aec503a
fernandaporocha <fernandaporocha@gmail.com>
2020-04-16 18:34:24 +0000

    Updates for listing the Episodes

--- mobdev-ca2/src/app/services/api.service.ts
+++ mobdev-ca2/src/app/services/api.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+    constructor(private http: HttpClient) { }
+
+    getEpisodes() {
+        return this.http.get('https://breakingbadapi.com/api/episodes')
+    }
+
+    getEpisode(id) {
+        return this.http.get('https://breakingbadapi.com/api/episodes/${id}')
+    }
 }
