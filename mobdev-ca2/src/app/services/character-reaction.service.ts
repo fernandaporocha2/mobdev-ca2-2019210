785cfc6f44e044f6931b14716da2aa2ecaaef037
Fernanda <fernandaporocha@gmail.com>
2020-04-26 20:28:56 +0000

    Adding comments to the services

--- mobdev-ca2/src/app/services/character-reaction.service.ts
+++ mobdev-ca2/src/app/services/character-reaction.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
 
+//Constants that will store the liked and disliked characters keys
 const LIKE_KEY = 'likedCharacters';
 const DISLIKE_KEY = 'dislikedCharacters';
 
@@ -11,12 +12,14 @@ export class CharacterReactionService {
 
   constructor(private storage: Storage) { }
 
+    //It return all the liked characters
     getLikedCharacters() {
         console.log("getLikedCharacters");
         console.log(JSON.stringify(this.storage.get(LIKE_KEY)));
         return this.storage.get(LIKE_KEY);
     }
 
+    //It return all the disliked characters
     getDislikedCharacters() {
         console.log("getDislikedCharacters");
         console.log(JSON.stringify(this.storage.get(DISLIKE_KEY)));
@@ -24,6 +27,7 @@ export class CharacterReactionService {
     }
 
 
+    //It verifies if a characterId is the liked list
     isLiked(characterId) {
         return this.getLikedCharacters().then(result => {
             console.log("isLiked");
@@ -32,6 +36,7 @@ export class CharacterReactionService {
         });
     }
 
+    //It verifies if a characterId is the disliked list
     isDisliked(characterId) {
         return this.getDislikedCharacters().then(result => {
             console.log("isDisliked");
@@ -40,54 +45,63 @@ export class CharacterReactionService {
         });
     }
 
+    //It likes a character - adds it to the liked list 
     likeCharacter(characterId) {
+        //For making sure it is not going to be in both list at the same time,
+        //the function removeFromDislikedCharacter is used
         this.removeFromDislikedCharacter(characterId);
         console.log("likeCharacter");
+        //It gets all the liked characters
         return this.getLikedCharacters().then(result => {
             if (result) {
+                //If the result is true, it means that there is an existent liked list already
+                //The characterId is add to result
                 result.push(characterId);
+                //It is saved
                 this.storage.set(LIKE_KEY, result);
-                console.log(JSON.stringify(this.storage.get(LIKE_KEY)));
-                console.log(JSON.stringify(this.storage.get(DISLIKE_KEY)));
                 return true;
             } else {
+                //If the result is false, it means that there is no liked list yet
+                //The characterId is saved in the liked list
                 this.storage.set(LIKE_KEY, [characterId]);
-                console.log(JSON.stringify(this.storage.get(LIKE_KEY)));
-                console.log(JSON.stringify(this.storage.get(DISLIKE_KEY)));
                 return true;
             }
         });
     }
 
+    //It dilikes a character - adds it to the disliked list 
     dislikeCharacter(characterId) {
+        //For making sure it is not going to be in both list at the same time,
+        //the function removeFromLikedCharacter is used
         this.removeFromLikedCharacter(characterId);
+        //It gets all the disliked characters
         return this.getDislikedCharacters().then(result => {
             if (result) {
+                //If the result is true, it means that there is an existent disliked list already
+                //The characterId is add to result
                 result.push(characterId);
+                //It is saved
                 this.storage.set(DISLIKE_KEY, result);
-                console.log(JSON.stringify("dislikeCharacter" + characterId));
-                console.log(JSON.stringify(this.storage.get(DISLIKE_KEY)));
-                return true;
             } else {
+                //If the result is false, it means that there is no dislike list yet
+                //The characterId is saved in disliked the list
                 this.storage.set(DISLIKE_KEY, [characterId]);
-                console.log(JSON.stringify("dislikeCharacter" + characterId));
-                console.log(JSON.stringify(this.storage.get(DISLIKE_KEY)));
                 return true;
             }
         });
     }
 
+    //When a character is liked or disliked it will be removed from both lists
     neutreCharacter(characterId) {
-        console.log("neutreCharacter"); 
         this.removeFromDislikedCharacter(characterId);
         this.removeFromLikedCharacter(characterId);
     }
 
+    //It removes a characterId from the liked list
     removeFromLikedCharacter(characterId) {
         return this.getLikedCharacters().then(result => {
+            //It verifies if the given characterId is in the liked list and removes it
             if (result &&  result.indexOf(characterId) !== -1) {   
-                console.log("removeFromLikedCharacte"); 
-                console.log(JSON.stringify(result.indexOf(characterId)));
                 var index = result.indexOf(characterId);
                 result.splice(index, 1);
                 return this.storage.set(LIKE_KEY, result);
@@ -95,11 +109,11 @@ export class CharacterReactionService {
         });
     }
 
+    //It removes a characterId from the disliked list
     removeFromDislikedCharacter(characterId) {
-        console.log("removeFromDislikedCharacte");
-        console.log(JSON.stringify(this.storage.get(DISLIKE_KEY)));
         return this.getDislikedCharacters().then(result => {
             console.log(JSON.stringify(result.indexOf(characterId)));
+            //It verifies if the given characterId is in the disliked list and removes it
             if (result && result.indexOf(characterId) !== -1) {
                 var index = result.indexOf(characterId);
                 result.splice(index, 1);
--- mobdev-ca2/src/app/services/episode-reaction.service.ts
+++ mobdev-ca2/src/app/services/episode-reaction.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
 
-
+//Constants that will store the liked and disliked episodes keys
 const LIKE_KEY = 'likedEpisodes';
 const DISLIKE_KEY = 'dislikedEpisodes';
 
@@ -12,19 +12,21 @@ export class EpisodeReactionService {
 
     constructor(private storage: Storage) { }
 
+    //It return all the liked episodes
     getLikedEpisodes() {
         console.log("getLikedEpisodes");
         console.log(JSON.stringify(this.storage.get(LIKE_KEY)));
         return this.storage.get(LIKE_KEY);
     }
 
+    //It return all the disliked episodes
     getDislikedEpisodes() {
         console.log("getDislikedEpisodes");
         console.log(JSON.stringify(this.storage.get(DISLIKE_KEY)));
         return this.storage.get(DISLIKE_KEY);
     }
 
-
+    //It verifies if an episodeId is the liked list
     isLiked(episodeId) {
         return this.getLikedEpisodes().then(result => {
             console.log("isLiked");
@@ -33,6 +35,7 @@ export class EpisodeReactionService {
         });
     }
 
+    //It verifies if an episodeId is the disliked list
     isDisliked(episodeId) {
         return this.getDislikedEpisodes().then(result => {
             console.log("isDisliked");
@@ -41,54 +44,64 @@ export class EpisodeReactionService {
         });
     }
 
+      //It likes an episode - adds it to the liked list 
     likeEpisode(episodeId) {
+         //For making sure it is not going to be in both list at the same time,
+        //the function removeFromDislikedEpisode is used
         this.removeFromDislikedEpisode(episodeId);
         console.log("likeEpisode");
+        //It gets all the liked episodes
         return this.getLikedEpisodes().then(result => {
             if (result) {
+                //If the result is true, it means that there is an existent liked list already
+                //The episodeId is add to result
                 result.push(episodeId);
+                //It is saved
                 this.storage.set(LIKE_KEY, result);
-                console.log(JSON.stringify(this.storage.get(LIKE_KEY)));
-                console.log(JSON.stringify(this.storage.get(DISLIKE_KEY)));
                 return true;
             } else {
+                //If the result is false, it means that there is no liked list yet
+                //The episodeId is saved in the liked list
                 this.storage.set(LIKE_KEY, [episodeId]);
-                console.log(JSON.stringify(this.storage.get(LIKE_KEY)));
-                console.log(JSON.stringify(this.storage.get(DISLIKE_KEY)));
                 return true;
             }
         });
     }
 
+    //It dilikes an episode - adds it to the disliked list 
     dislikeEpisode(episodeId) {
+        //For making sure it is not going to be in both list at the same time,
+        //the function removeFromLikedEpisode is used
         this.removeFromLikedEpisode(episodeId);
+        //It gets all the disliked episodes
         return this.getDislikedEpisodes().then(result => {
             if (result) {
+                //If the result is true, it means that there is an existent disliked list already
+                //The episodeId is add to result
                 result.push(episodeId);
+                //It is saved
                 this.storage.set(DISLIKE_KEY, result);
-                console.log(JSON.stringify("dislikeEpisode" + episodeId));
-                console.log(JSON.stringify(this.storage.get(DISLIKE_KEY)));
                 return true;
             } else {
+                //If the result is false, it means that there is no dislike list yet
+                //The episodeId is saved in disliked the list
                 this.storage.set(DISLIKE_KEY, [episodeId]);
-                console.log(JSON.stringify("dislikeEpisode" + episodeId));
-                console.log(JSON.stringify(this.storage.get(DISLIKE_KEY)));
                 return true;
             }
         });
     }
 
+    //When an episode is liked or disliked it will be removed from both lists
     neutreEpisode(episodeId) {
-        console.log("neutreEpisode"); 
         this.removeFromDislikedEpisode(episodeId);
         this.removeFromLikedEpisode(episodeId);
     }
 
+    //It removes an episodeId from the liked list
     removeFromLikedEpisode(episodeId) {
         return this.getLikedEpisodes().then(result => {
+            //It verifies if the given characterId is in the liked list and removes it
             if (result &&  result.indexOf(episodeId) !== -1) {   
-                console.log("removeFromLikedEpisode"); 
-                console.log(JSON.stringify(result.indexOf(episodeId)));
                 var index = result.indexOf(episodeId);
                 result.splice(index, 1);
                 return this.storage.set(LIKE_KEY, result);
@@ -96,11 +109,11 @@ export class EpisodeReactionService {
         });
     }
 
+    //It removes an episodeId from the disliked list
     removeFromDislikedEpisode(episodeId) {
-        console.log("removeFromDislikedEpisode");
-        console.log(JSON.stringify(this.storage.get(DISLIKE_KEY)));
         return this.getDislikedEpisodes().then(result => {
             console.log(JSON.stringify(result.indexOf(episodeId)));
+            //It verifies if the given characterId is in the disliked list and removes it
             if (result && result.indexOf(episodeId) !== -1) {
                 var index = result.indexOf(episodeId);
                 result.splice(index, 1);
